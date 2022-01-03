8d946d329362f82ff0928a1369844312fce31a1d
viniciussaibweb <viniciuscruz@saibweb.com.br>
2022-01-03 15:42:24 -0300

    componente header pre construido

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,20 @@
-import Document, { Head, Main, Html, NextScript } from "next/document";
+import Document, { Head, Main, Html, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Main />
-        <NextScript />
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap"
+						rel="stylesheet"
+					/>
+					<link rel="shortcut icon" href="favicon.png" type="image/png" />
+				</Head>
+				<Main />
+				<NextScript />
+			</Html>
+		);
+	}
 }
